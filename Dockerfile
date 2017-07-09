1960be5c02e0908a4a98bcff2f1b2f7b7418bcdc
Carolyn Van Slyck <me@carolynvanslyck.com>
2017-07-09 18:07:24 -0500

    Improve preview performance

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,7 @@
 FROM ruby:2.3.1
 
+RUN apt-get update && apt-get install -y rsync
+
 COPY Gemfile /tmp
 COPY Gemfile.lock /tmp
 RUN cd /tmp && bundle install --jobs 3 --retry 3
--- scripts/serve.sh
+++ scripts/serve.sh
@@ -6,10 +6,13 @@ ROOT="$( cd "$( dirname "${BASH_SOURCE[0]}" )/.." && pwd )"
 echo "Loading minikube's kubectl config"
 kubectl config set current-context minikube
 
+echo "HACK: Removing old deployment..."
+kubectl delete -f $ROOT/manifests/deployment.yaml &> /dev/null || true
+
 echo "Deploying carolynvanslyck.com to the cluster"
 kubectl apply -f $ROOT/manifests/deployment.yaml
 kubectl apply -f $ROOT/manifests/service.yaml
 kubectl apply -f $ROOT/manifests/ingress.yaml
 
-echo "Site ready at http://carolynvanslyck.mini -> $(minikube service cvs --url)"
-open http://carolynvanslyck.mini
+echo "Site ready at $(minikube service cvs --url)"
+open $(minikube service cvs --url)
